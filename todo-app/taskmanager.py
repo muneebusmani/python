1dcceebd3c475e223635d34b45cebb8345673e14
muneeb usmani <muneebusmani8355@gmail.com>
2024-11-25 18:46:28 +0500

    Refactored code, Working on adding id system

--- todo-app/taskmanager.py
+++ todo-app/taskmanager.py
@@ -2,6 +2,91 @@ import json
 from tabulate import tabulate
 import os
 
+
+if "tasks.json" not in os.listdir(os.getcwd()):
+    with open("tasks.json", "x") as f:
+        json.dump([], f, indent=4)
+
+
+def load_tasks():
+    clear_terminal()
+    f = open("tasks.json", "r")
+    tasks = json.load(f)
+    f.close
+    return tasks
+
+
+# Function for saving tasks
+def save_tasks(tasks):
+    with open("tasks.json", "w") as f:
+        json.dump(tasks, f, indent=4)
+    clear_terminal()
+
+
+def clear_terminal():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def create_task():
+
+    # Input the Name of Task
+    task = input(
+        """
+        enter task name:
+        """
+    )
+
+    # Input the Description of Task
+    description = input(
+        """
+        enter task description
+        """
+    )
+
+    # Load all tasks
+    data = load_tasks()
+
+    # Append currently created task to existing tasks
+    data.append({"task": task, "description": description})
+
+    # Write the appended json to tasks.json file
+    save_tasks(data)
+    list_tasks()
+
+
+def list_tasks():
+
+    # clear the command line
+    clear_terminal()
+
+    # Load all Tasks
+    data = load_tasks()
+
+    # Empty Dictionary for creating rows
+    table_data = []
+
+    # Looping through each object{} in dictionary[]
+    for task in data:
+
+        # Adding values of each task in each row for that table data
+        table_data.append(
+            [
+                # This fetches the values for the specified keys and if the value isn't found, it defaults to empty string ""
+                task.get("id", ""),
+                task.get("task", ""),
+                task.get("description", ""),
+            ]
+        )
+
+        # Now finally printing that key less data with only values and headers from tablute library
+        print(
+            tabulate(
+                table_data,
+                headers=["Id", "Task", "Description"],
+            )
+        )
+
+
 while True:
     selection = int(
         input(
@@ -12,61 +97,6 @@ while True:
         )
     )
     if selection == 1:
-        task = input(
-            """
-        enter task name:
-        """
-        )
-        description = input(
-            """
-        enter task description
-        """
-        )
-        task_json = json.dumps({"task": task, "description": description})
-
-        if "tasks.json" not in os.listdir(os.getcwd()):
-            open("tasks.json", "x")
-
-        f = open("tasks.json", "w")
-        f.write(task_json)
-        f.close()
+        create_task()
     elif selection == 2:
-        with open("tasks.json", "r") as file:
-            data = json.load(file)
-            table_data = []
-            for task in data:
-                table_data = table_data.append(
-                    [
-                        task.get("id", ""),
-                        task.get("name", ""),
-                        task.get("description", ""),
-                    ]
-                )
-            print(
-                tabulate(
-                    table_data,
-                    headers=["Id", "Task", "Description"],
-                )
-            )
-
-            # data = {"name": "lol", "description": "lol"}
-            # table_data = [
-            #     {data.get("name"), data.get("description")},
-            #     {data.get("name"), data.get("description")},
-            # ]
-            # print(
-            #     tabulate(
-            #         [
-            #             {
-            #                 1,
-            #                 "House chores",
-            #                 "do all the chores and make sure everything is done",
-            #             },
-            #             {
-            #                 2,
-            #                 "House chores",
-            #                 "do all the chores and make sure everything is done",
-            #             },
-            #         ],
-            #     )
-            # )
+        list_tasks()
